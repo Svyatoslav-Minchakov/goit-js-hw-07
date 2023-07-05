f4a95eaaca2d2cccd2191ecbdc06ba5ce368b3e7
Святослав Минчаков <sminchakov@gmail.com>
2023-07-06 01:23:02 +0300

    HW was added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListArray = galleryItems.map((item) => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+}).join('');
+
+let instance;
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', galleryListArray);
+
+galleryContainer.addEventListener('click', openImage);
+galleryContainer.addEventListener('keydown', modalClose);
+
+function openImage(event) {
+
+  if(event.target.tagName !== 'IMG') {
+    return 
+  }
+  const source = event.target.dataset.source;
+  event.preventDefault()
+  instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+  `)
+  instance.show()
+}
+
+function modalClose(event) {
+  if (event.key === "Escape") {
+    instance.close();
+  }
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryItemsArray = galleryItems.map(item => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>
+</li>`
+}).join('');
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsArray);
+
+galleryContainer.addEventListener('click', openImage);
+
+
+let lightbox;
+
+function openImage(event) {
+  event.preventDefault();
+  if(event.target.tagName !== 'IMG') return;
+  lightbox = new SimpleLightbox('.gallery a');
+  galleryContainer.removeEventListener('click', openImage);
+}
+
+
